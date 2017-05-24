9b252ce965094e207c56bb1e17106e222ae69c1f
= <lcf33123@foxmail.com>
2017-05-25 00:18:33 +0800

    加入vuex，修改路由

--- src/router/index.js
+++ src/router/index.js
@@ -1,25 +1,47 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Hello from '@/components/Hello'
-import Home from '@/components/Home'
-import Login from '@/components/Login'
+import App from '../App'
+
+import store from '../store'
+
+// import Hello from '@/components/Hello'
+// import Home from '@/components/Home'
+// import Login from '@/components/Login'
 
 Vue.use(Router)
 
-export default new Router({
+const router = new Router({
   routes: [
     {
-      path: '/hello',
-      name: 'Hello',
-      component: Hello
-    }, {
       path: '/',
-      name: 'Home',
-      component: Home
-    }, {
-      path: '/login',
-      name: 'login',
-      component: Login
+      name: 'Hello',
+      component: App,
+      children: [
+        {
+          path: '/login',
+          meta: {auth: false},
+          component: resolve => require(['../components/Login'], resolve)
+        }, {
+          path: '/home',
+          component: resolve => require(['../components/Home'], resolve)
+        }, {
+          path: '*', // 其他页面强制跳转登录页
+          redirect: '/login'
+        }
+      ]
     }
   ]
 })
+
+router.beforeEach(({meta, path}, form, next) => {
+  var {auth = true} = meta
+  var isLogin = Boolean(store.state.user.id)
+  // var isLogin = true
+
+  if (auth && !isLogin && path !== '/login') {
+    return next({path: '/login'})
+  }
+  next()
+})
+
+export default router
